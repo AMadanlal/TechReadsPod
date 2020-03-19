b76ac07b4beccd3012dd121e0f4caec8381ab403
Akshar Madanlal <AMadanlal@jhb.dvt.co.za>
2020-03-19 16:18:47 +0200

    Creating Bridging class and one protocol (but still need to connect it)

---
+++ TechReadsPod/DisplayProtocol.h
@@ -0,0 +1,17 @@
+//
+//  DisplayProtocol.h
+//  TechReadsPod
+//
+//  Created by Akshar Madanlal on 2020/03/19.
+//  Copyright © 2020 DVT. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@protocol DisplayProtocol
+- (void) todisplay;
+@end
+
+NS_ASSUME_NONNULL_END
