5b95bc512aa55a0ec2baae9660360e9bf3b6141d
Akshar Madanlal <AMadanlal@jhb.dvt.co.za>
2020-03-24 11:05:52 +0200

    attempting another write function

--- TechReadsPod/Preferences/ViewModel/SavingUtilities.m
+++ TechReadsPod/Preferences/ViewModel/SavingUtilities.m
@@ -32,6 +32,9 @@ return homeDir;
   return filetext;
 }
 
+//@param str String to write.
+//@param fileName Name of the file.
+
 - (void) SaveMedium : (NSMutableString*) tosave {
 //  filepath = [[NSString alloc] init];
 //  filepath = [self.GetDocumentDirectory stringByAppendingPathComponent: @"UserMedium.txt"];
@@ -40,13 +43,28 @@ return homeDir;
 //  if (!isOK) {
 //    NSLog(@"Error writing file at %@\n%@",
 //    filepath, [err localizedFailureReason]);
-//  }
-  NSString *docPath = [NSHomeDirectory() stringByAppendingPathComponent:@"Documents/UserMedium.txt"];
-  [tosave writeToFile:docPath
-            atomically:YES
-              encoding:NSUTF8StringEncoding
-                 error:NULL];
-}
+  NSData *data = [tosave dataUsingEncoding:NSUTF8StringEncoding];
+      NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+      if (0 < [paths count]) {
+          NSString *documentsDirPath = [paths objectAtIndex:0];
+          NSString *filePath = [documentsDirPath stringByAppendingPathComponent:@"UserMedium.txt"];
+          NSFileManager *fileManager = [NSFileManager defaultManager];
+          if ([fileManager fileExistsAtPath:filePath]) {
+              // Add the text at the end of the file.
+              NSFileHandle *fileHandler = [NSFileHandle fileHandleForUpdatingAtPath:filePath];
+  //            [fileHandler seekToEndOfFile];
+              [fileHandler writeData:data];
+              [fileHandler closeFile];
+          } else {
+              // Create the file and write text to it.
+              [data writeToFile:filePath atomically:YES];
+          }
+      }
+  
+  }
+ 
+
+
 
 @end
 NS_ASSUME_NONNULL_END
