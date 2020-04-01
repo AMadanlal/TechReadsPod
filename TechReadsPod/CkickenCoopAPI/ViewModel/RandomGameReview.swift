6c7c876910cbff06e376722364823d804a5c1d70
Akshar Madanlal <AMadanlal@jhb.dvt.co.za>
2020-04-01 12:58:41 +0200

    done the requested changes

--- TechReadsPod/CkickenCoopAPI/ViewModel/RandomGameReview.swift
+++ TechReadsPod/CkickenCoopAPI/ViewModel/RandomGameReview.swift
@@ -13,33 +13,32 @@ public class RandomGameReview {
 
   public func getRandomGameFromList(completionHandler: @escaping(GameListItem) -> Void) {
     let utillities = SavingUtilities.init()
-    var listItem = GameListItem()
-    var compatableGameList = [GameListItem]()
-    let platformSearch = utillities.loadMedium()
-    let apiClass = ChickenCoopAPI(searched: "", platform: platformSearch ?? "pc")
-    apiClass.getGameList { result in
-      switch result {
-      case.failure(let error):
-        print(error)
-      case.success(let details):
-        let gameList = details
-        DispatchQueue.main.async {
-          for item in gameList.result where item.platform == platformSearch {
-              compatableGameList.append(item)
-          }
-          if compatableGameList.count == 1 {
-             listItem = compatableGameList[0]
-          } else if compatableGameList.count > 1 {
-            let randomInt = Int.random(in: 0..<compatableGameList.count)
-  //            insert random game from the list here
-            listItem = compatableGameList[randomInt]
-          } else if compatableGameList.count == 0 {
-            let randomInt = Int.random(in: 0..<gameList.result.count)
-            listItem  = gameList.result[randomInt]
-          }
-          completionHandler(listItem)
-        }
-      }
-    }
+       var listItem = GameListItem()
+       var compatableGameList = [GameListItem]()
+       let platformSearch = utillities.loadMedium()
+       let apiClass = ChickenCoopAPI(searched: "", platform: platformSearch ?? "pc")
+       apiClass.getGameList { result in
+         switch result {
+         case.failure(let error):
+           print(error)
+         case.success(let details):
+           let gameList = details.result
+           DispatchQueue.main.async {
+             for item in gameList where item.platform == platformSearch {
+                 compatableGameList.append(item)
+             }
+             if compatableGameList.count == 1 {
+               listItem = compatableGameList.first ?? GameListItem()
+             } else if compatableGameList.count > 1 {
+               let randomInt = Int.random(in: 0..<compatableGameList.count)
+     //            insert random game from the list here
+               listItem = compatableGameList[randomInt]
+             } else if compatableGameList.isEmpty {
+               listItem  = gameList.randomElement() ?? GameListItem()
+             }
+             completionHandler(listItem)
+           }
+         }
+       }
   }
 }
